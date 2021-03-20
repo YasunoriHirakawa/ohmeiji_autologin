35d7d76c555084df77dd33ab0570d69ef347c18d
YasunoriHirakawa <60866331+YasunoriHirakawa@users.noreply.github.com>
2021-03-21 01:12:21 +0900

    Create autologin.js

---
+++ autologin.js
@@ -0,0 +1,14 @@
+var id = document.getElementsByName("ACCOUNTUID")[0];
+if(id){
+    id.value = "YOUR ID";
+}
+
+var passwd = document.getElementsByName("PASSWORD")[0];
+if(passwd){
+    passwd.value = "YOUR PASSWORD";
+}
+
+var btn = document.getElementsByName("SUBMIT")[0];
+if(btn){
+    btn.click();
+}
